842be4ae03a818abdf41dcbf94f2034331788120
ZYKDX <101040143+ZYKDX@users.noreply.github.com>
2022-06-07 09:44:35 -0700

    Update problem1.java

--- problem1.java
+++ problem1.java
@@ -5,23 +5,42 @@
 public class PizzaOrder {
 
     private int numPizzas;
-    private Pizza pizza1 = new Pizza();
-    private Pizza pizza2 = new Pizza();
-    private Pizza pizza3 = new Pizza();
+    private Pizza pizza1 = null;
+    private Pizza pizza2 = null;
+    private Pizza pizza3 = null;
 
     /**
      * Construct a pizza order and initialized number of pizza in the order to be 1
      */
-    public PizzaOrder(){
-        numPizzas = 1;
-    }
+    public PizzaOrder() {numPizzas = 1;}
 
     /**
      * Construct a pizza order and initialized the number of pizzas in the order to the given value
      * @param numPizzas number of pizzas in the order (1,2,or 3)
      */
-    public PizzaOrder(int numPizzas){
-        setNumPizzas(numPizzas);
+    public PizzaOrder(int numPizzas) {setNumPizzas(numPizzas);}
+
+    /**
+     * this is a copy constructor
+     */
+    public PizzaOrder(PizzaOrder otherOrder)
+    {
+        this.numPizzas = otherOrder.numPizzas;
+        this.pizza1 = null;
+        this.pizza2 = null;
+        this.pizza3 = null;
+        if(this.numPizzas > 0)
+        {
+            this.pizza1 = new Pizza(otherOrder.pizza1);
+        }
+        if(this.numPizzas > 1)
+        {
+            this.pizza2 = new Pizza(otherOrder.pizza2);
+        }
+        if(this.numPizzas > 2)
+        {
+            this.pizza3 = new Pizza(otherOrder.pizza3);
+        }
     }
 
     /**
@@ -42,16 +61,21 @@ public class PizzaOrder {
      * Get the number of pizzas in the order
      * @return the number of pizzas in the order
      */
-    public int getNumPizzas(){
-        return numPizzas;
-    }
+    public int getNumPizzas() {return numPizzas;}
 
     /**
      * Set the first pizza in the order to the given object
      * @param pizza1 the pizza information we want our 1st pizza to be
      */
     public void setPizza1(Pizza pizza1){
-        this.pizza1 = pizza1;
+        if(numPizzas > 0)
+        {
+            this.pizza1 = pizza1;
+        }
+        else
+        {
+            System.out.println("Set pizza fail: Invalid number of pizzas in the order!");
+        }
     }
 
     /**
@@ -59,7 +83,14 @@ public class PizzaOrder {
      * @return the information of the 1st pizza in the order
      */
     public Pizza getPizza1(){
-        return pizza1;
+        if(numPizzas > 0)
+        {
+            return pizza1;
+        }
+        else
+        {
+            return null;
+        }
     }
 
     /**
@@ -67,7 +98,14 @@ public class PizzaOrder {
      * @return the information of the second pizza in the order
      */
     public Pizza getPizza2(){
-        return pizza2;
+        if(numPizzas > 1)
+        {
+            return pizza2;
+        }
+        else
+        {
+            return null;
+        }
     }
 
     /**
@@ -75,7 +113,14 @@ public class PizzaOrder {
      * @return the information of the third pizza in the order
      */
     public Pizza getPizza3(){
-        return pizza3;
+        if(numPizzas > 2)
+        {
+            return pizza3;
+        }
+        else
+        {
+            return null;
+        }
     }
 
     /**
@@ -83,7 +128,14 @@ public class PizzaOrder {
      * @param pizza2 the pizza information we want our 2nd pizza to be
      */
     public void setPizza2(Pizza pizza2){
-        this.pizza2 = pizza2;
+        if(numPizzas > 1)
+        {
+            this.pizza2 = pizza2;
+        }
+        else
+        {
+            System.out.println("Set pizza fail: Invalid number of pizzas in the order!");
+        }      
     }
 
     /**
@@ -91,7 +143,14 @@ public class PizzaOrder {
      * @param pizza3 the pizza information we want our 3rd pizza to be
      */
     public void setPizza3(Pizza pizza3){
-        this.pizza3 = pizza3;
+        if(numPizzas > 2)
+        {
+            this.pizza3 = pizza3;
+        }
+        else
+        {
+            System.out.println("Set pizza fail: Invalid number of pizzas in the order!");
+        } 
     }
 
     /**
@@ -119,26 +178,21 @@ public class PizzaOrder {
      * Main method which is used to do some manual test of the class.
      * @param args
      */
-    public static void main(String[] args){
-        Pizza pizza1 = new Pizza("large", 1, 0, 1);
-        Pizza pizza2 = new Pizza("medium", 2,2,0);
-        Pizza pizza3 = new Pizza();
-        PizzaOrder order = new PizzaOrder();
-        order.setNumPizzas(2);
-        order.setPizza1(pizza1);
-        order.setPizza2(pizza2);
-        double total = order.calcTotal();
-        System.out.println("There are " + order.numPizzas + " pizza(s) in the order\n");
-        System.out.println("The 1st pizza:");
-        System.out.println(pizza1.getDescription());
-        if(order.numPizzas == 2){
-            System.out.println("The 2nd pizza:");
-            System.out.println(pizza2.getDescription());
-        }
-        if(order.numPizzas == 3){
-            System.out.println("The 3rd pizza:");
-            System.out.println(pizza3.getDescription());
-        }
-        System.out.printf("Total cost of the order: %.2f\n", total);
+    public static void main(String[] args){   
+       Pizza pizza1 = new Pizza("large", 1, 0, 1);
+       Pizza pizza2 = new Pizza("medium", 2, 2, 0);
+       PizzaOrder order1 = new PizzaOrder();
+       order1.setNumPizzas(2);
+       order1.setPizza1(pizza1);
+       order1.setPizza2(pizza2);
+       double total1 = order1.calcTotal();
+       PizzaOrder order2 = new PizzaOrder(order1);
+       order2.getPizza1().setCheese(3);
+       double total2 = order2.calcTotal();
+       double origTotal = order1.calcTotal();
+       System.out.println(total1);
+       System.out.println(total2);
+       System.out.println(origTotal);
     }
 }
+
